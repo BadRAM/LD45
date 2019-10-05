818802e19de9ec6205ae4e0e598f92d3855ab557
BadRAM <dgamebillda@gmail.com>
2019-10-04 22:28:18 -0700

    wip commit to pull capsule movement.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private float Health;
+    private NavMeshAgent _agent;
+    private EnemyWeapon _weapon;
+    private EnemyAI _AI;
+    private Vector3 _playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void FixedUpdate()
+    {
+        _agent.SetDestination(_playerPos);
+    }
+
+    public void Damage(int DamageTaken)
+    {
+        Health -= DamageTaken;
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //activate death particles, disable functional bits.
+    }
+}
---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    
+    
+    // default EnemyAI moves towards the player
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
