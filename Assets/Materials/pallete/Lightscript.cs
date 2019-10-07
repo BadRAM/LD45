da5035aca6f6bf17e6a24a2f65c7b541ec0d70c9
phie <vixianphimmasone@gmail.com>
2019-10-07 09:41:05 -0700

    Main menu
    Main menu Screen done with scene transition to game. Change official Game scene to scene number 1 in build settings.
    
    added bullet mesh

---
+++ Assets/Materials/pallete/Lightscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightscript : MonoBehaviour
+{
+    public Light LocaleLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LocaleLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LocaleLight.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Scenemanagement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenemanagement : MonoBehaviour
+{
+    public bool Menuscreen;
+    public Light TheLight;
+
+    Ray MouseDetect;
+    RaycastHit hit;
+
+    private void Start()
+    {
+        TheLight = TheLight.GetComponent<Light>();
+    }
+    private void Update()
+    {
+        
+        if (Menuscreen == true)
+        {
+            
+            MouseDetect = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(MouseDetect,out hit))
+            {
+                //print(hit.collider.name);
+                if(hit.collider.name == "BTN1")
+                {
+                    TheLight.enabled = true;
+                    TheLight.color = Color.green;
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        LoadScene(1);
+                    }
+                }
+                else
+                {
+                    TheLight.enabled = false;
+                }
+
+                if (hit.collider.name == "BTN2")
+                {
+                    TheLight.enabled = true;
+                    TheLight.color = Color.red;
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Application.Quit();
+                    }
+                }
+            }
+        }
+    }
+  
+  
+
+    void LoadScene(int SceneNumber)
+    {
+        SceneManager.LoadScene(SceneNumber, LoadSceneMode.Single);
+    }
+}
